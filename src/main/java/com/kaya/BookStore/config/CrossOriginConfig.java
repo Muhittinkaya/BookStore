8111adfeb922836ead64e834da297e58e3b20de1
Muhittinkaya <mhttnkaya@gmail.com>
2024-03-17 01:51:31 +0300

    add crossorigin

--- src/main/java/com/kaya/BookStore/config/CrossOriginConfig.java
+++ src/main/java/com/kaya/BookStore/config/CrossOriginConfig.java
@@ -1,10 +1,12 @@
 package com.kaya.BookStore.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@CrossOrigin
 public class CrossOriginConfig implements WebMvcConfigurer {
 
     @Override
